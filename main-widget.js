481267e07711321c92b78f20c2f5dbe78d9ecbcb
Soshi Tsunashima <22724971+hellososhi@users.noreply.github.com>
2024-07-15 16:59:12 +0900

    [add] image description generation using OpenAI API

--- main-widget.js
+++ main-widget.js
@@ -9,6 +9,8 @@ reearth.ui.show(`
     }
     #wrapper {
       height: 100%;
+      max-height: 600px;
+      overflow-y: auto;
       box-sizing: border-box;
       border-radius:12px;
       padding: 12px;
@@ -36,22 +38,53 @@ reearth.ui.show(`
     document.getElementById("button").addEventListener("click", (e) => {
       parent.postMessage({
         type: "captureScreen",
-        openaiApiKey: document.getElementById("openai-api-key").value
       }, "*");
     });
 
     addEventListener("message", e => {
       if (e.source !== parent) return;
       if (e.data.type && e.data.type === 'getCaptureScreen') {
+        const capturedImage = e.data.payload;
         const img = document.createElement("img");
-        img.src = e.data.payload;
+        img.src = capturedImage;
         document.getElementById("wrapper").appendChild(img);
+        fetch("https://api.openai.com/v1/chat/completions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + document.getElementById("openai-api-key").value
+          },
+          body: JSON.stringify({
+            "model": "gpt-4o",
+            "messages": [
+              {
+                "role": "user",
+                "content": [
+                  {
+                    "type": "text",
+                    "text": "この画像について説明してください。"
+                  },
+                  {
+                    "type": "image_url",
+                    "image_url": {
+                      "url": capturedImage
+                    }
+                  }
+                ]
+              }
+            ],
+            "max_tokens": 300
+          })
+        }).then(res => res.json()).then((data) => {
+          const response = data.choices[0].message.content;
+          document.getElementById("wrapper").appendChild(document.createTextNode(response));
+        })
       }
     })
   </script>
 `);
 
-reearth.on("message", msg => {
+reearth.on("message", (msg) => {
   if (msg.type === "captureScreen") {
     reearth.ui.postMessage({
       type: "getCaptureScreen",
